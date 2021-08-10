47bba619ddc41bb8fc15011467e77e6c9e10a520
Aaron Ennis <aaron@aaronennis.com>
2021-08-09 22:52:32 -0700

    Add factory constructor for FoodWastePost class

--- lib/models/food_waste_post.dart
+++ lib/models/food_waste_post.dart
@@ -13,4 +13,14 @@ class FoodWastePost {
       required this.quantity,
       required this.latitude,
       required this.longitude});
+
+  factory FoodWastePost.fromJSON(Map<String, dynamic> json) {
+    return FoodWastePost(
+      date: json["date"],
+      imageUrl: json["imageUrl"],
+      quantity: int.parse(json["quantity"]),
+      latitude: json["latitude"],
+      longitude: json["longitude"],
+    );
+  }
 }

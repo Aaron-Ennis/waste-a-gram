dc42379e4d3b1c2150f47da69f4a9b2ce20f7c90
Aaron Ennis <aaron@aaronennis.com>
2021-08-11 13:57:34 -0700

    Clean up some code and implement loading indicator between screens

--- lib/helpers/get_image.dart
+++ lib/helpers/get_image.dart
@@ -6,7 +6,10 @@ Future getImage() async {
   File? image;
   final imagePicker = ImagePicker();
   final file = await imagePicker.pickImage(source: ImageSource.gallery);
-  image = File(file!.path);
+  if (file == null) {
+    return null;
+  }
+  image = File(file.path);
   var filename = DateTime.now().toString() + ".jpg";
   Reference storageRef = FirebaseStorage.instance.ref().child(filename);
   UploadTask uploadTask = storageRef.putFile(image);
--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,13 +1,10 @@
-import "dart:io";
 import "package:cloud_firestore/cloud_firestore.dart";
 import "package:flutter/material.dart";
-import "package:firebase_storage/firebase_storage.dart";
-import "package:image_picker/image_picker.dart";
 
 // internally-maintained imports
 import "../helpers/formatted_date.dart";
-import "../widgets/dynamic_title.dart";
-import "../widgets/upload_button.dart";
+import "../helpers/get_image.dart";
+import "../screens/new_post_screen.dart";
 
 class ListScreen extends StatefulWidget {
   const ListScreen({Key? key}) : super(key: key);
@@ -17,23 +14,34 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
-  File? image;
-  final imagePicker = ImagePicker();
-  int totalWaste = 0;
+  late bool loading;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      loading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     TextStyle? style = Theme.of(context).textTheme.headline6;
-    return Scaffold(
-      appBar: AppBar(
-        title: DynamicTitle(),
-        centerTitle: true,
-      ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection("posts").snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasData && snapshot.data!.docs.length > 0) {
-            return ListView.builder(
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection("posts").snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasData && snapshot.data!.docs.length > 0 && !loading) {
+          // Calculate the total waste to display in the AppBar
+          int totalWaste = 0;
+          snapshot.data!.docs.forEach((doc) {
+            totalWaste += doc["quantity"] as int;
+          });
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Waste-A-Gram-" + totalWaste.toString()),
+              centerTitle: true,
+            ),
+            body: ListView.builder(
               itemCount: snapshot.data!.docs.length,
               itemBuilder: (context, index) {
                 var post = snapshot.data!.docs[index];
@@ -47,14 +55,46 @@ class _ListScreenState extends State<ListScreen> {
                   ),
                 );
               },
-            );
-          } else {
-            return (Center(child: CircularProgressIndicator()));
-          }
-        },
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: onPressed,
+              child: Icon(Icons.camera_alt),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  // There is probably a cleaner way to trigger the Circular Progress
+  // Indicator than using a setState bool when waiting for the async return.
+  // This works for now, but need to research...
+  void onPressed() async {
+    setState(() {
+      loading = true;
+    });
+
+    final url = await getImage();
+
+    setState(() {
+      loading = false;
+    });
+
+    if (url == null) {
+      return;
+    }
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => NewPostScreen(imageUrl: url),
       ),
-      floatingActionButton: UploadButton(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }

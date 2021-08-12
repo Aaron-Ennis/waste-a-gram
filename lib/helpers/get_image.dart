ef64bf90111b40ebecd48a5ac4366c968adcf0ef
Aaron Ennis <aaron@aaronennis.com>
2021-08-12 15:10:46 -0700

    Cleap up pubspec.yaml

--- lib/helpers/get_image.dart
+++ lib/helpers/get_image.dart
@@ -2,6 +2,9 @@ import "dart:io";
 import "package:firebase_storage/firebase_storage.dart";
 import "package:image_picker/image_picker.dart";
 
+// This helper function opens the device's image gallery for selection
+// and uploads the image to cloud storage. It then returns the URL for
+// the uploaded file
 Future getImage() async {
   File? image;
   final imagePicker = ImagePicker();
